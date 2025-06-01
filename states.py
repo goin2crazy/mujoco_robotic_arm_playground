1689be6e821e6d6bbffe94fbde4bfc0c0b465e11
Double Cringe <yanyap666@gmail.com>
2025-06-01 14:14:20 +0300

    Positive reward fn From 5-distance To min(20, 1/distance))

--- states.py
+++ states.py
@@ -77,7 +77,7 @@ def reward_function_grasp(model, data):
     distance_sum = distance_fingers_egg_right + distance_fingers_egg_left
 
     # Original reward calculation logic
-    reward += ( 5 - distance_sum)
+    reward += min(20 , 1/distance_sum)
 
     # In python True in bool = 1 and False = 0, this behavior is preserved.
     # Assuming egg_on_the_floor returns a boolean or 0/1.
